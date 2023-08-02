d3f9e54b31f325f41be93930ccb17c8099e3fd8d
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2023-08-02 15:30:54 -0100

    SPM#6: Configure project to run unit tests with Less framework

---
+++ __mocks__/@chuva.io/less.js
@@ -0,0 +1,17 @@
+module.exports = {
+    topics: new Proxy({}, {
+        get: () => {
+            return {
+                publish: async (message) => undefined
+            }
+        }
+    }),
+
+    sockets: new Proxy({}, {
+        get: () => {
+            return {
+                publish: async (message) => undefined
+            }
+        }
+    })
+};
